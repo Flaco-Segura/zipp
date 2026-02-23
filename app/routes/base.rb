ac4d5171def1424f84cc6cc34bbd5cc59dbd2023
Flaco-Segura <manuelquesadasegura@gmail.com>
2026-02-23 20:34:45 +0100

    add logo

--- app/routes/base.rb
+++ app/routes/base.rb
@@ -4,6 +4,7 @@ class BaseRoutes < Sinatra::Base
   configure do
     set :root, File.expand_path('../../', __dir__)
     set :views, File.expand_path('../../app/views', __dir__)
+    set :public_folder, File.expand_path('../../public', __dir__)
     enable :sessions
     set :session_secret, ENV.fetch('SESSION_SECRET', 'a_super_secret_key_for_development_that_is_at_least_sixty_four_characters_long_for_rack_session_to_be_happy')
   end
